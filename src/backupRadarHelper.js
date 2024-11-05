bdc2a49e506e8bf0dd3830f9a9a93979be4fd2f8
jbart178 <jbart178190@gmail.com>
2024-11-05 23:09:59 +1000

    added comments regarding how to access data

---
+++ src/backupRadarHelper.js
@@ -0,0 +1,9 @@
+/**
+ * Use scheduled reports for backup radar
+ * Read csv from reports@atlanticdigital.com.au when it arrives and then use *
+ * data to build report.
+ * Scheduled repoorts are already set up, would need to change format to not
+ * pdf (probably csv) then read csv and generate report format
+ *
+ * https://us.backupradar.com/app/reporting/scheduled
+ */
---
+++ src/ninjaHelper.js
@@ -0,0 +1,9 @@
+/**
+ * Use ninja API or use email reports
+ * For API https://app.ninjarmm.com/apidocs/?links.active=core#/queries/getInstalledOSPatches
+ * - /v2/queries/os-patch-installs
+ * - /v2/queries/os-patches
+ * - /v2/organization/{id}/devices
+ *
+ * Alternatively, can probably request a report in ninja to reports@atlanticdigital.com.au and parse os patching data from that report
+ */
